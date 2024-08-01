30c6ba17ddf203c5720c33ac209a41eab792d84f
Ranjith Khumar Shanmugasundaram <r.shanmugasundaram@wikki-gmbh.de>
2024-08-01 17:22:02 +0200

    Added structfoam

---
+++ src/solvers/structFoam/structureFoam.C
@@ -0,0 +1,114 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | foam-extend: Open Source CFD
+   \\    /   O peration     | Version:     4.1
+    \\  /    A nd           | Web:         http://www.foam-extend.org
+     \\/     M anipulation  | For copyright notice see file Copyright
+-------------------------------------------------------------------------------
+License
+    This file is part of foam-extend.
+
+    foam-extend is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    foam-extend is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with foam-extend.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    structure is a modified version of solidDisplacementFoam
+
+Description
+    Transient segregated finite-volume solver of the Biot quasi-steady consolidation equations
+    for linear-elastic, small-strain deformation of a solid skeleton coupled with
+    pore water flow and pressure governed by Darcy's law.
+
+    Simple linear elasticity structural analysis code.
+    Solves for the displacement vector field D and the pore water pressure p.
+    Also generating the stress tensor field sigma.
+
+    An additional laplacian equation is solved in order to calculate the accumulated
+    pore pressure pE.
+Author
+    R. Shanmugasundaram, Wikki GmbH
+    Johan Roenby, DHI Water & Environment
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "zeroGradientFvPatchFields.H"
+#include "constitutiveModel.H"
+#include "wallDist.H"
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+    #include "setRootCase.H"
+
+    #include "createTime.H"
+    #include "createMesh.H"
+    #include "readMaterialProperties.H"
+    #include "readPoroElasticControls.H"
+    #include "createFields.H"
+
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    Info<< "\nCalculating displacement field\n" << endl;
+
+    while (runTime.loop())
+    {
+        Info<< "Iteration: " << runTime.value() << nl << endl;
+
+        #include "readPoroElasticControls.H"
+
+        int iCorr = 0;
+        scalar UResidual = 1.0e10;
+        scalar pResidual = 1.0e10;
+        scalar residual = 1.0e10;
+
+        U.correctBoundaryConditions();
+        p.correctBoundaryConditions();
+
+        #include "updateValues.H"
+        do
+        {
+            Info << "iCorr ="<< iCorr << endl;
+            U.storePrevIter();
+            p.storePrevIter();
+            sigmaD.correctBoundaryConditions();
+
+            #include "pEqn.H"
+
+            #include "UEqn.H"
+
+            residual = max(pResidual,UResidual);
+
+        } while (residual > convergenceTolerance && ++iCorr < nCorr);
+
+        Info << "number of iterations " << iCorr << endl;
+
+        #include "calculateStress.H"
+
+        #include "prepareBuildup.H"
+
+
+
+        Info<< "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
+            << "  ClockTime = " << runTime.elapsedClockTime() << " s"
+            << nl << endl;
+    }
+
+    Info<< "End\n" << endl;
+
+    return 0;
+}
+
+
+// ************************************************************************* //
